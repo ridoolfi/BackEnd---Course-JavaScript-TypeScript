296d587e4340a3a57eaf762d27ac3cc5e17b4d7e
ridoolfi <angeloggc2@gmail.com>
2023-10-12 23:27:08 -0300

    'AulasRealizadas'

--- aula6/index.js
+++ aula6/index.js
@@ -5,10 +5,22 @@ console.log(`${nome} nasceu em 1984.`);
 console.log(`Em 2000 ${nome} conheceu Maria.`);
 console.log(`${nome} casou-se com maria em 2012.`);
 console.log(`Maria teve um filho com ${nome} em 2015.`);
+
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uima váriavel com um número <= Da erro de syntax
+
 console.log(`O filho de ${nome} se chama Eduardo.`);
 
 
 nome = 'Otávio'
+
+// Nome das váriaveis não podem conter espaços ou traços
+// Utilizamos camelCase para fazer váriaveis com duas palavras
+// Case-sensitive <- letras minusculas e maiusculas importam na hora de criar uma variável
+// Não podemos redeclarar váriaveis com let
+// Não utilize var, UTILIZE LET. <- Tudo que for variar é let
 
 console.log(`${nome} nasceu em 1984.`);
 console.log(`Em 2000 ${nome} conheceu Maria.`);
---
+++ aula7/index.js
@@ -0,0 +1,20 @@
+// Váriavel constante
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uima váriavel com um número <= Da erro de syntax
+// Nome das váriaveis não podem conter espaços ou traços
+// Utilizamos camelCase para fazer váriaveis com duas palavras
+// Case-sensitive <- letras minusculas e maiusculas importam na hora de criar uma variável
+// Não podemos modificar os valores de uma constante
+// Constantes devem ser criadas e inicializadas com um valor
+
+const nome = 'João';
+
+console.log(nome)
+
+//nome = 'Lucas';    <== Não pode alterar o valor
+/*
+Se fosse para alterar, teria que iniciar com Let ao invés do const
+*/
+
+console.log(nome)
