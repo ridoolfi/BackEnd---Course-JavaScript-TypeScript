74cbdf54df94f5013fc7d5fc13f0461c04165cb7
ridoolfi <angeloggc2@gmail.com>
2023-11-24 21:14:28 -0300

    authUser Class

---
+++ aula11/js/script.js
@@ -0,0 +1,3 @@
+let num1 = parseFloat(window.prompt('Informe um número:'))
+let num2 = parseFloat(window.prompt('Informe outro número: '))
+alert(`A soma entre ${num1} e ${num2} é igual a ${num1 + num2}`)
---
+++ aula13/strings.js
@@ -0,0 +1,12 @@
+let umaString = "O rato roeu a roupa do rei de roma.";    //<= Barra invertida escapa o caractere com aspas iguais dentro de uma string
+/*console.log(umaString.lastIndexOf('m', 3))
+console.log(umaString + ' em um lindo dia')
+console.log(`${umaString} em um lindo dia`)
+console.log(umaString.search(/x/))
+console.log(umaString.replace(/r/g, '#'))
+console.log(umaString.substring(umaString.length - 5, umaString.length - 1))*/
+
+
+console.log(umaString.split(' ', 2))
+
+
